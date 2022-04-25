0218ba5d4c4b63e2955f872c85946f9aaea5dbeb
kraszor <kraszewski111@gmail.com>
2022-04-25 12:41:18 +0200

    hosts change

--- photos/settings.py
+++ photos/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", 'https://iphotos-pap.herokuapp.com']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", 'iphotos-pap.herokuapp.com']
 
 
 # Application definition
