e3e198c6fb8f3aa03835ddbfb86c9710127baf48
kraszor <kraszewski111@gmail.com>
2022-04-25 13:19:41 +0200

    staticfiles collecting

--- photos/settings.py
+++ photos/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import environ
 from pathlib import Path
 import django_heroku
 import dj_database_url
-from decouple import config
+# from decouple import config
+import environ
 import os
 
 
