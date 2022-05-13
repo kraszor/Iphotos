9b13535eae300d988d54a60c957d20a0e38b6d4b
kraszor <kraszewski111@gmail.com>
2022-05-13 17:15:32 +0200

    basic api implementation

--- photo/urls.py
+++ photo/urls.py
@@ -11,7 +11,13 @@ urlpatterns = [
     path('download/', views.download, name='download'),
     path('download_app', views.export, name='download_app'),
     path('group/create/', views.GroupCreate.as_view(), name='create_group'),
-    path('groups', views.GroupsView.as_view(), name='groups')
+    path('groups', views.GroupsView.as_view(), name='groups'),
+    path('api/', views.api_overview),
+    path('api/groups', views.groups_list),
+    path('api/places', views.places_list),
+    path('api/images', views.images_list),
+    path('api/photos', views.photos_list),
+    path('api/groups/<int:pk>/', views.group_detail),
 
 
 ]
