266fdbf5a3053a7db97dda48c4519ddf68b4e57f
kraszor <kraszewski111@gmail.com>
2022-05-15 18:54:52 +0200

    update of frontend for displaying groups

--- photo/models.py
+++ photo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import F
 from django.urls import reverse
 
 # Create your models here.
@@ -7,9 +8,9 @@ from django.urls import reverse
 
 class UsersGroup(models.Model):
     # owner = models.CharField(max_length=100, default=None, editable=False)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+', default=None, editable=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+', default=1, editable=False)
     name = models.fields.CharField(max_length=150)
-    photo_count = models.fields.IntegerField(default=0)
+    photo_count = models.fields.IntegerField(default=0, editable=False)
     tags = models.fields.CharField(max_length=200, default="empty")
     color_group = models.fields.CharField(max_length=50, default="unknown")
     is_local = models.fields.BooleanField(default=False)
@@ -40,8 +41,14 @@ class Photo(models.Model):
     title = models.fields.CharField(max_length=150)
     tags = models.fields.CharField(max_length=300)
     date_taken = models.fields.DateTimeField()
-    album = models.ForeignKey(UsersGroup, on_delete=models.CASCADE)
+    album = models.ForeignKey(UsersGroup, on_delete=models.CASCADE, related_name='photos')
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
     place = models.ForeignKey(Place, on_delete=models.SET_DEFAULT, default='Unknown')
+
+    def save(self, *args, **kwargs):
+        super().save(self,*args, **kwargs)
+        group = UsersGroup.objects.get(pk=self.album.id)
+        group.photo_count += 1
+        group.save()
 
 
